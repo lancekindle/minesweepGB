1729bc18a2e887449c75dfb163e113576f353571
Lance Kindle <lance.kindle@gmail.com>
2016-11-30 02:33:20 -0800

    add lda & ifa, improve preserve and if_flag(s)
    adds "all" to preserve keyword to preserve all registers
    add lda, a small macro to load into A.
    add ifa, a sleek macro to make number comparisons with A super easy and
    readable.
    add jr_inverse, a macro to jr with the inverse of the passed flag. It's
    being used to significantly improve if_flag(s).
    if_flag(s) are now just as fast (computationally) as if_not_flag(s), so
    use whichever reads easier.
    add preserve2, allowing you to preserve two register pairs at once.
    rename expand_arg*_cmd to unpack_arg*_cmd like python's naming scheme.

--- syntax.asm
+++ syntax.asm
@@ -27,7 +27,6 @@
 	IF	!DEF(SYNTAX_ASM)
 SYNTAX_ASM	SET	1
 
-
 ; simple macros to set/return true or false. Use these everywhere!
 ret_true: MACRO
 	set_true
@@ -46,66 +45,106 @@ set_false: MACRO
 	CCF     ; compliment carry flag. (toggle it's value)
 	ENDM
 
+lda: MACRO
+	ld	a, \1
+	ENDM
 
 ; if_* MACROs take 2 arguments.
 ; 1) a function to call. Depending on if it returns true or not,
 ; 2) a command to run depending on return condition of function
+; technically, the 2nd arg can be many arguments. They'll all get appended
+; to the end of the 2nd arg run. (see unpack_arg2_cmd for details)
 if_: MACRO
 	call \1
 	jr nc, .end_if_\@
-	expand_arg2_cmd
+	unpack_arg2_cmd
 .end_if_\@
 	ENDM
 
 if_not: MACRO
 	call \1
 	jr c, .end_if_not\@
-	expand_arg2_cmd
+	unpack_arg2_cmd
 .end_if_not\@
 	ENDM
+
+; macro for comparing contents of a to another value.
+; can compare to: another register (b, c, d, e, h, l), a hard-coded value,
+; or [HL]. [HL] means the byte to which HL points
+; in assembly, the command `cp x` compares the register / value x to A:
+; if A == x, Z=1, C=0
+; if A > x, Z=0, C=0
+; if A < x, Z=0, C=1
+; this macro smartly determines what type of comparison you want, and uses only
+; those instructions. So it's a sleek, fast and readable way to compare #s
+ifa: MACRO
+	cp \2
+	IF (STRCMP("\1", "==") == 0) ; Z=1
+	jr	nz, .skip_if_a\@
+	ENDC
+	IF (STRCMP("\1", ">=") == 0) ; C=0
+	jr	c, .skip_if_a\@
+	ENDC
+	IF (STRCMP("\1", ">") == 0) ; C=0 & Z=0
+	jr	c, .skip_if_a\@
+	jr	z, .skip_if_a\@
+	ENDC
+	IF (STRCMP("\1", "<=") == 0) ; C=1 | Z=1
+	jr	c, .exec_if_a_cmd\@  ; immediately exec if C=1
+	jr	nz, .skip_if_a\@  ; fallthrough if Z=1. Otherwise, skip
+	ENDC
+	IF (STRCMP("\1", "<") == 0) ; C=1
+	jr 	nc, .skip_if_a\@
+	ENDC
+	IF (STRCMP("\1", "!=") == 0) ; Z=0
+	jr	z, .skip_if_a\@
+	ENDC				; support both != and <>
+	IF (STRCMP("\1", "<>") == 0) ; Z=0
+	jr	z, .skip_if_a\@
+	ENDC
+.exec_if_a_cmd\@
+	unpack_arg3_cmd
+.skip_if_a\@
+	ENDM
+
 
 ; first argument is a flag condition, such as z, nz, c, nc
 ; second argument is a command to run if flag is NOT set
 if_not_flag: MACRO
 	jr \1, .end_if_not_flag\@
-	expand_arg2_cmd
+	unpack_arg2_cmd
 .end_if_not_flag\@
 	ENDM
+
+; first argument is a flag condition, such as z, nz, c, nc
+; second argument is a command to run if flag IS SET
+if_flag: MACRO
+	jr_inverse	\1, .end_if_flag\@
+	unpack_arg2_cmd
+.end_if_flag\@
+	ENDM
+
 
 ; first two arguments are flag condition, 3rd argument is command to run
 if_not_flags: MACRO
 	jr \1, .end_if_not_flags\@
 	jr \2, .end_if_not_flags\@
-	expand_arg3_cmd
+	unpack_arg3_cmd
 .end_if_not_flags\@
 	ENDM
 
-; between the two, if_not_flag is faster than if_flag.
-; but readability counts, man. Use if_flag when that's what you're checking for
-; Later, when revising for speed, you can switch to if_not_flag
-
-
-; first argument is a flag condition, such as z, nz, c, nc
-; second argument is a command to run if flag IS SET
-if_flag: MACRO
-	if_not_flag	\1, jr .end_if_flag\@
-	expand_arg2_cmd
-.end_if_flag\@
-	ENDM
-
 if_flags: MACRO
-	if_not_flag	\1, jr .end_if_flags\@
-	if_not_flag	\2, jr .end_if_flags\@
-	expand_arg3_cmd
+	jr_inverse	\1, .end_if_flags\@
+	jr_inverse	\2, .end_if_flags\@
+	unpack_arg3_cmd
 .end_if_flags\@
 	ENDM
-
 
 
 ; first of all, let me apologize that this mess below is nearly the first thing
 ; you see in this file. But it plays an integral part to my macros. The a2-a9
 ; strings are one-line macros that get joined into the overall one-line macro
-; `expand_arg2_cmd`. This is responsible for handling all command arguments
+; `unpack_arg2_cmd`. This is responsible for handling all command arguments
 ; passed into other macros. In general, this means it'll handle arguments
 ; \2 - \9 (if that many exist). This allows the user to specify some pretty
 ; complex commands, like calling another macro with multiple arguments if an
@@ -145,8 +184,9 @@ a6	EQUS	"IF _NARG==7\n \\2,\\3,\\4,\\5,\\6,\\7\nENDC\n"
 a7	EQUS	"IF _NARG==8\n \\2,\\3,\\4,\\5,\\6,\\7,\\8\nENDC\n"
 a8	EQUS	"IF _NARG==9\n \\2,\\3,\\4,\\5,\\6,\\7,\\8,\\9\nENDC\n"
 
-expand_arg2_cmd	EQUS	"{a1}{a2}{a3}{a4}{a5}{a6}{a7}{a8}"
+unpack_arg2_cmd	EQUS	"{a1}{a2}{a3}{a4}{a5}{a6}{a7}{a8}"
 
+; and of course, some macros need to expand arg3+, so here it is
 b2	EQUS	"IF _NARG==3\n \\3\nENDC\n"
 b3	EQUS	"IF _NARG==4\n \\3,\\4\nENDC\n"
 b4	EQUS	"IF _NARG==5\n \\3,\\4,\\5\nENDC\n"
@@ -155,31 +195,77 @@ b6	EQUS	"IF _NARG==7\n \\3,\\4,\\5,\\6,\\7\nENDC\n"
 b7	EQUS	"IF _NARG==8\n \\3,\\4,\\5,\\6,\\7,\\8\nENDC\n"
 b8	EQUS	"IF _NARG==9\n \\3,\\4,\\5,\\6,\\7,\\8,\\9\nENDC\n"
 
-expand_arg3_cmd	EQUS	"{b2}{b3}{b4}{b5}{b6}{b7}{b8}"
+unpack_arg3_cmd	EQUS	"{b2}{b3}{b4}{b5}{b6}{b7}{b8}"
 
 
-; preserve & restore register pair after fxn call. \1 can be: af, bc, de, hl
+; jr_inverse is just like jr, except it tests for the opposite flag
+; i.e. jr_inverse c, rDMA ==> jr nc, rDMA
+; this is most useful in the if_flag macro
+jr_inverse: MACRO
+	IF (STRCMP("\1", "c") == 0)
+	jr	nc, \2
+	ENDC
+	IF (STRCMP("\1", "nc") == 0)
+	jr	c, \2
+	ENDC
+	IF (STRCMP("\1", "z") == 0)
+	jr	nz, \2
+	ENDC
+	IF (STRCMP("\1", "nz") == 0)
+	jr	z, \2
+	ENDC
+	IF (STRCMP("\1", "z") != 0) && (STRCMP("\1", "c") != 0)
+		IF (STRCMP("\1", "nc") != 0) && (STRCMP("\1", "nz") != 0)
+			FAIL "\n\1 is not a valid flag\n"
+		ENDC
+	ENDC
+	ENDM
+
+
+; preserve & restore register pair after cmd. \1 can be: af, bc, de, hl, or all
+; if \1 is "all", then all registers will be preserved
 ; \2 is FULL command. such as CALL xyz or a macro. 
 preserve: MACRO
+	IF (STRCMP("\1", "all") == 0)
+		push af
+		push bc
+		push de
+		push hl
+	ELSE
+		push \1
+	ENDC
+	unpack_arg2_cmd
+	IF (STRCMP("\1", "all") == 0)
+		pop hl
+		pop de
+		pop bc
+		pop af
+	ELSE
+		pop \1
+	ENDC
+	ENDM
+
+preserve2: MACRO
 	push \1
-	expand_arg2_cmd
+	push \2
+	unpack_arg3_cmd
+	pop \2
 	pop \1
 	ENDM
 
 
 ; an attempt to allow outside access to this macro
 expand_arg2: MACRO
-	expand_arg2_cmd
+	unpack_arg2_cmd
 	ENDM
 
 
 ; setvar variable, value
-; uses a register. So preserve if necessary
+; uses  registers. So preserve if necessary
 setvar: MACRO
 	ld a, \2
 	ld [\1], a
 	ENDM
 ;getvar is simply ld a, [var]
-
 
 	ENDC  ; end syntax file
