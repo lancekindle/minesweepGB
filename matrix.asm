1b79cf63ef49f99a12c77d8bd5bf4a7d2298c630
Lance Kindle <lance.kindle@gmail.com>
2016-12-14 03:16:17 -0800

    Add ability to create matrix at any defined address
    VERY useful to address VRAM tiles as a matrix.
    Checking if passed in variable name is already defined allows
    us to instead point the matrix to the already defined address
    and use the mat_* macros.

--- matrix.asm
+++ matrix.asm
@@ -16,11 +16,27 @@ MATRIX_ASM	SET	1
 ; create a matrix named `arg1` of Y, X size (arg2 and arg3, respectively)
 ; the matrix name will also have two other variables created, which simply
 ; attach _Y and _X to the name of the matrix
+; In order to allow treating other ram-space and VRAM as a matrix, I also
+; detect if the user is passing an already-defined variable. In that case,
+; I append _Y and _X to the variable name, but leave the value of the
+; variable unchanged. This allows one to pass in _SCRN0 (with a value of $9800)
+; into mat_Create and begin using all the mat_* macros to address _SCRN0 as
+; a matrix.
 mat_Create: MACRO
 \1_Y = \2
 \1_X = \3
-	var_MidRamBytes	\1, (\2 * \3)	; allocate memory for matrix
-	; \1 now equals the starting memory address of its matrix
+	IF !DEF(\1)	; this will raise "syntax error" if \1 is just a #
+		PRINTT "\nCreating Matrix called \1 with (H, W) = (\2, \3)\n"
+		var_MidRamBytes	\1, (\2 * \3)	; allocate memory for matrix
+		; \1 now equals the starting memory address of its matrix
+	ELSE
+		PRINTT "\nCreating Matrix \1 @ "
+		PRINTV	\1
+		PRINTT	" and (H, W) = (\2, \3)"
+		; we actually don't need to do anything else here. Matrix is
+		; "set up" in that \1_Y and \1_X are now defined. mat_GetYX
+		; and others will now just work.
+	ENDC
 	ENDM
 
 
