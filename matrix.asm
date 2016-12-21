cf426d979f0a79377d1740ed88bfaed4b59bee5f
Lance Kindle <lance.kindle@gmail.com>
2016-12-21 03:03:13 -0800

    replace AddressYX with Index
    use GetIndex and SetIndex to set matrix value at given index.
    This method is much faster than AddressYX because Index calculation
    is the same as AddressYX (multiplication), but Index can be re-used
    to reference another matrix of the same dimensions with very little
    computational overhead. (Index is simply added to the Matrix's
    starting address, vs GetYX and SetYX re-calculate Index each time).

--- matrix.asm
+++ matrix.asm
@@ -40,22 +40,12 @@ mat_Create: MACRO
 	ENDM
 
 
-; place the memory address for matrix (arg1) @ Y, X into HL
-; calculates it as	Matrix_Address + X + (Y * width)
-; Y in register A, X in register E
-; because register E doesn't get squashed by math_Mult
-; this macro squashes all registers (sorry)
-mat_AddressYX: MACRO
-	load	a, \2, "mat_* arg2 is Y-coordinate"
-	load	d, 0
-	; \3 is X addr. So we setup DE equal to $0X  (where X is X-coordinate)
-	load	e, \3, "mat_* arg3 is X-coordinate"
-	push	de
-	math_Mult	a, \1_X	; calculate Y mem offset (into HL)
-	pop	de
-	add	hl, de	; add X mem offset to HL
-	load	bc, \1
-	add	hl, bc	; add matrix address to HL offset
+
+mat_GetIndex: MACRO
+	load	bc, \1, "arg1 of mat_GetIndex is matrix-name or address"
+	load	hl, \2, "arg2 of mat_GetIndex is address-offset or HL"
+	add	hl, bc		; compute location of matrix @ offset
+	ld	a, [HL]	; load value from matrix @ offset into a
 	ENDM
 
 ; usage:
@@ -66,9 +56,23 @@ mat_AddressYX: MACRO
 ; make sure you use the matrix variable name here
 mat_GetYX: MACRO
 	load	d, \2, "mat_GetYX arg2 is Y-coordinate in matrix"
-	ld	a, d	; swap Y into register A
-	mat_AddressYX	\1, a, \3
+	load	e, \3, "mat_GetYX arg3 is X-coordinate in matrix"
+	mat_IndexYX	\1, d, e	; place index@YX in HL
+	ld	bc, \1	; load base address of matrix
+	add	hl, bc	; add base addr of matrix to Index
 	ld	a, [HL]	; load matrix@YX into a
+	ENDM
+
+
+; set value @ memory offset specified. the offset must be hard-coded or
+; in register HL. Value to set is in register A (or a hard-coded #)
+; mat_SetIndex		matrix_name, $00ff, 32
+mat_SetIndex: MACRO
+	load	bc, \1, "arg1 of mat_SetIndex is matrix-name"
+	load	hl, \2, "arg2 of mat_SetIndex is address-offset"
+	load	a, \3, "arg3 of mat_SetIndex is value to place in matrix"
+	add	hl, bc		; compute location of matrix @ Index
+	ld	[HL], a		; load value into matrix @ Index
 	ENDM
 
 ; usage:
@@ -78,13 +82,37 @@ mat_GetYX: MACRO
 ; value is either a # or register A
 mat_SetYX: MACRO
 	; load'ing of vars MUST happen first (aka... what if we pass a?)
-	load	a, \4, "arg4 of mat_SetYX is value to set in matrix"
+	load	bc, \1, "mat_SetYX arg1 is name or base-address of matrix"
 	load	d, \2, "mat_SetYX arg2 is Y-coordinate in matrix"
-	push af
-	ld	a, d	; swap Y into register A
-	mat_AddressYX	\1, a, \3
-	pop af
+	load	e, \3, "mat_SetYX arg3 is X-coordinate in matrix"
+	load	a, \4, "arg4 of mat_SetYX is value to set in matrix"
+	push	af
+	push	bc
+	mat_IndexYX	\1, d, e	; place index@YX in HL
+	pop	bc
+	add	hl, bc	; add base addr of matrix to Index
+	pop	af
 	ld	[HL], a		; load value (a) into matrix@Y,X
 	ENDM
 
 
+; use this macro to calculate the address Index. Useful for manipulating
+; several matrices at once by pre-calculating the Index / offset
+; (And then just adding the address of the matrix itself to begin the
+; set/get operation)
+; mat_GetAddressIndex	matrix_name, Y, X
+; offset will be stored in HL
+mat_IndexYX: MACRO
+	load	d, \2, "mat_* arg2 is Y-coordinate in matrix"
+	; \3 is X addr. So we setup DE == $0X  (X is x-coordinate)
+	load	e, \3, "mat_* arg3 is X-coordinate in matrix"
+	ld	a, d	; push Y-coordinate into a for multiplication
+	ld	d, 0	; DE is now just X-coordinate
+	push	de
+	math_Mult	a, \1_X	; calculate Y mem offset (into HL)
+	pop	de
+	add	hl, de	; add X mem offset to HL
+	ENDM
+
+
+	ENDC	; end matrix define
