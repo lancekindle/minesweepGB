a15bcf01c76eb23d0e0fc96bb65097ce5ac9a790
Lance Kindle <lance.kindle@gmail.com>
2016-12-27 02:59:00 -0800

    add Iterator fxn and macros
    (also change matrix _Y, _X to _H, _W)
    Iterators must be declared, then Initialized before they can be used.
    (declare reserves ram space, initialize sets them). Init also sets
    the values for iterating the full matrix. Eventually macros should be
    added to allow iteration over a submatrix within the full matrix.
    IterNext returns with the next byte in A, and it's corresponding
    address in HL. An IterStop is signalled by a cleared Carry-Flag (0).
    IterStop indicates that you've already got the last element in the
    previous iteration. So it's wise to "jp nc, xxxx" RIGHT after calling
    mat_IterNext to avoid re-using the repeated value
    IterStop will still return with the last element in A and it's
    address in HL

--- matrix.asm
+++ matrix.asm
@@ -13,18 +13,18 @@ include "math.asm"
 MATRIX_ASM	SET	1
 ; max matrix dimension allowed is 255
 
-; create a matrix named `arg1` of Y, X size (arg2 and arg3, respectively)
-; the matrix name will also have two other variables created, which simply
-; attach _Y and _X to the name of the matrix
+; declare a matrix named `arg1` of H, W size (arg2 and arg3, respectively)
+; the matrix name will also have two other variables declared, which simply
+; attach _H and _X to the name of the matrix
 ; In order to allow treating other ram-space and VRAM as a matrix, I also
 ; detect if the user is passing an already-defined variable. In that case,
-; I append _Y and _X to the variable name, but leave the value of the
+; I append _H and _X to the variable name, but leave the value of the
 ; variable unchanged. This allows one to pass in _SCRN0 (with a value of $9800)
-; into mat_Create and begin using all the mat_* macros to address _SCRN0 as
+; into mat_Declare and begin using all the mat_* macros to address _SCRN0 as
 ; a matrix.
-mat_Create: MACRO
-\1_Y = \2
-\1_X = \3
+mat_Declare: MACRO
+\1_H = \2
+\1_W = \3
 	IF !DEF(\1)	; this will raise "syntax error" if \1 is just a #
 		PRINTT "\nCreating Matrix called \1 with (H, W) = (\2, \3)\n"
 		var_MidRamBytes	\1, (\2 * \3)	; allocate memory for matrix
@@ -34,12 +34,170 @@ mat_Create: MACRO
 		PRINTV	\1
 		PRINTT	" and (H, W) = (\2, \3)"
 		; we actually don't need to do anything else here. Matrix is
-		; "set up" in that \1_Y and \1_X are now defined. mat_GetYX
+		; "set up" in that \1_H and \1_W are now defined. mat_GetYX
 		; and others will now just work.
+		; DONT FORGET TO INITIALIZE VALUES IN MATRIX
 	ENDC
 	ENDM
 
 
+; initialize matrix fully with a particular value (hard-coded or in A)
+mat_Init: MACRO
+	load	hl, \1		; address of matrix
+	load	a, \2		; value to set
+	load	bc, \1_H * \1_W	; size of matrix
+	IF (\1 < $A000) && (\1 + (\1_H * \1_W) > $8000)
+	; if inside VRAM space ($8000 - $A000), use SetVRAM to prevent
+	; access outside of vblank (else it won't work or will trash video)
+		call	mem_SetVRAM
+	ELSE
+		call	mem_Set
+	ENDC
+	ENDM
+
+mat_IterDeclare: MACRO
+	var_LowRamByte	\1_iter_ptr	; pointer to (sub)matrix index
+	var_LowRamByte	\1_iter_ptr2	; msb of matrix pointer
+	var_LowRamByte	\1_iter_steps_remain	; steps remaining in column
+	var_LowRamByte	\1_iter_step		; iterator step in x
+	var_LowRamByte	\1_iter_rowsteps_remain	; row steps remaining
+	var_LowRamByte	\1_iter_rowstep		; iterator step in y (from end of submatrix)
+	var_LowRamByte	\1_iter_W	; W of submatrix we're iterating
+					; (used to reset \1_iter_step_remain
+	ENDM				; after we advance a row)
+
+
+; initialize iterator. Save variables to ram
+; if you pass in no arguments (aside from matrix name) the iterator will be
+; set up to iterate the entire matrix
+; variables are set up one-after another in ram so that they can be read
+; from [de] pointer, increment de, then read next variable
+mat_IterInit: MACRO
+	IF _NARG == 1
+	ld	bc, \1 - 1	; load address of matrix - 1
+				; (we increment before sampling)
+	lda	c	; load LSB of matrix address (-1)
+	ld	[\1_iter_ptr], a
+	lda	b	; load MSB of matrix address (-1)
+	ld	[\1_iter_ptr2], a
+	; store steps-remain as a way to count down until we reach end of row
+	; in this case, the width of the matrix -1
+	lda	\1_W - 1
+	ld	[\1_iter_steps_remain], a
+	; store width of matrix as iter_W - 1. If we were iterating a submatrix
+	; the width would be smaller than the full matrix. This number is used
+	; to restore _iter_step_remain when we advance to the next row
+	; (it's also equal to the initial value of \1_iter_steps remain)
+	ld	[\1_iter_W], a
+	; step is amount that we increment index-address (iter_ptr) each time
+	; we advance to the next element
+	lda	1
+	ld	[\1_iter_step], a	; store step (in x-direction) as 1
+	; store rowstep remain as a way to count down # of rows to traverse
+	; (in this case, the height of the matrix - 1)
+	lda	\1_H - 1
+	ld	[\1_iter_rowsteps_remain], a
+	; rowstep is extra amount to increment to return to start of next row
+	; for a whole-matrix iteration, rowstep is 1, as that'll get us from
+	; the last element of row 1 to the first element of row2, for example.
+	lda	1
+	ld	[\1_iter_rowstep], a	; store rowstep of 1
+	ENDC
+	ENDM
+
+; call this macro to get the next element in an initiated matrix iterator
+; the next-in-line byte will be in A when this completes, and HL will contain
+; the matrix index, the address where the byte A was retrieved.
+; Carry flag will be reset (to 0) when the iterator has finished. The element
+; in A once StopIteration is signalled is a repeat of the last element.
+; (since technically, you've finished iterating the matrix BEFORE you get the
+; StopIteration signal).
+mat_IterNext: MACRO
+	; load pointer to current index within matrix
+	ld	de, \1_iter_ptr	; de points to LSB of address ptr to submatrix
+	call	mat_IterNext_fxn
+	ENDM
+
+
+; iterate matrix, returning next-in-line element in A.
+; (retrieves iteration variables starting from HL)
+; ALWAYS returns with valid element in A
+; and HL will contain memory address at which element was taken
+; if user attempts to call IterNext and the iterator has already
+; reached the end, it will reset the carry-flag (to 0) and return the last
+; element once again. This is known as the IterStop Signal.
+; If IterNext_fxn is called again after it has already reached the end,
+; it will return the last valid element from iteration,
+; and reset carry flag (to 0), as if it had just reached the end.
+; this is NOT the same thing as "Index". If you want to alter the value
+; at address HL, simply use	ld [HL], xxx
+; where xxx can be a register or a hard-coded value. Just be very careful
+; that you verify it's not within VRAM range. (If it is, you just need to wait
+; for V-Blank before writing, or else you risk garbling the display).
+mat_IterNext_fxn:
+	ld	b, 0	; setup for adding bc to HL later ;)
+	push	de	; store "start of variables" address
+	; retrieve index address from ram at de+1, de => H,L
+	ld	a, [de]
+	ld	l, a
+	increment	de
+	ld	a, [de]
+	ld	h, a	; now hl points to curent index in (sub)matrix
+.iter_next_step
+	increment	de	; now DE points to \1_iter_steps_remain
+	ld	a, [de]
+	and	a	; set Z flag if steps (in column) remaining is zero
+	jr	z, .iter_next_row	; skip iter-col to begin iter-row
+	dec	a
+	ld	[de], a		; update value of \1_iter_steps_remain in ram
+	increment	de	; point to \1_iter_step
+	ld	a, [de]
+	ld	c, a		; Assume b=0. So BC=step-amount
+	add	hl, bc			; step to next element in iterator
+	jr	.iter_get_element
+.iter_next_row	;we get here if we reached end of matrix row.
+	increment	de	; point to \1_iter_step (was skipped above)
+	increment	de	; point to \1_iter_rowsteps_remain
+	ld	a, [de]
+	and	a	; set Z flag if rows remaining is zero
+	; if no rows remain, we haven't got any more elements to read
+	; which means we already got last element in the last iteration
+	jr	z, .iter_ended	; hl points to last element
+	dec	a	; ah, good. continue to next row's element
+	ld	[de], a	; store remaining rows
+	increment	de	; points to \1_iter_rowstep
+	ld	a, [de]		; assume b=0. So BC=rowstep-amount
+	ld	c, a
+	increment	de	; points to \1_iter_W
+	ld	a, [de]		; grab reset # for \1_iter_steps_remain
+	add	hl, bc	; step to next row's element in iterator
+	; now we need to reset column-steps-remaining
+	pop	de
+	push	de	; retrieve (and re-store) address of \1_iter_ptr
+	increment	de	; point to \1_iter_ptr2
+	increment	de	; point to \1_iter_steps_remain
+	ld	[de], a		; reset \1_iter_steps_remain in ram
+	jr	.iter_get_element
+.iter_ended
+	pop	de ; retrieve pushed (now unnecessary) address of variables
+	ld	a, [hl]		;get element @ index
+	set_false		; indicate end of iteration.
+	ret			; No new element was retried this iteration
+				; The last element is repeated here
+.iter_get_element
+	ld	c, [hl]			; get element @ index
+	pop	de		; retrieve "start of variables" address
+	ld	a, l
+	ld	[de], a		; store LSB of index pointer
+	ld	a, h
+	increment	de
+	ld	[de], a		; store MSB. Finishes storing new index in ram
+	ld	a, c		; place element in A
+	set_true		; indicate there are still more elements
+	ret
+
+
+	
 
 mat_GetIndex: MACRO
 	load	bc, \1, "arg1 of mat_GetIndex is matrix-name or address"
@@ -109,7 +267,7 @@ mat_IndexYX: MACRO
 	ld	a, d	; push Y-coordinate into a for multiplication
 	ld	d, 0	; DE is now just X-coordinate
 	push	de
-	math_Mult	a, \1_X	; calculate Y mem offset (into HL)
+	math_Mult	a, \1_W	; calculate Y mem offset (into HL)
 	pop	de
 	add	hl, de	; add X mem offset to HL
 	ENDM
