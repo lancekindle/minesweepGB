b5882bf8145e1cf4777e482b8c763309ad5dceff
Lance Kindle <lance.kindle@gmail.com>
2016-12-28 03:48:03 -0800

    improve Init macros
    IterInit can now accept hard-loaded or dynamically loaded values from
    a register. mat_Init now checks for an address within VRAM and calls
    the correct VRAM memory copy functions. mat_Init also can initialize
    a matrix from preset values defined in the rom or elsewhere by
    specifying the starting address and bytcount. Super useful for
    hard-definining an initial matrix that's visually easy to understand.

--- matrix.asm
+++ matrix.asm
@@ -25,6 +25,7 @@ MATRIX_ASM	SET	1
 mat_Declare: MACRO
 \1_H = \2
 \1_W = \3
+\1_size = \2 * \3
 	IF !DEF(\1)	; this will raise "syntax error" if \1 is just a #
 		PRINTT "\nCreating Matrix called \1 with (H, W) = (\2, \3)\n"
 		var_MidRamBytes	\1, (\2 * \3)	; allocate memory for matrix
@@ -42,22 +43,65 @@ mat_Declare: MACRO
 
 
 ; initialize matrix fully with a particular value (hard-coded or in A)
+; OR
+; initialize a matrix from a hard-coded matrix defined in the ROM.
+; it's most easily done by applying a pre and post label like so:
+; matrix_start:
+; DB	1, 2, 3
+; DB	4, 5, 6
+; DB	7, 8, 9
+; matrix_end:
+; then you can initialize a matrix (called tictac) to those values like so:
+; mat_Declare	tictac, 3, 3
+; mat_Init	tictac, matrix_start, matrix_end - matrix_start
+; tictac will now hold the values 1,2,3,4,5,6,7,8,9 in ram (in matrix format)
 mat_Init: MACRO
-	load	hl, \1		; address of matrix
-	load	a, \2		; value to set
-	load	bc, \1_H * \1_W	; size of matrix
-	IF (\1 < $A000) && (\1 + (\1_H * \1_W) > $8000)
-	; if inside VRAM space ($8000 - $A000), use SetVRAM to prevent
-	; access outside of vblank (else it won't work or will trash video)
-		call	mem_SetVRAM
-	ELSE
-		call	mem_Set
+	IF _NARG == 2
+		load	hl, \1		; address of matrix
+		load	a, \2		; value to set
+		load	bc, \1_H * \1_W	; size of matrix
+		IF (\1 < $A000) && (\1 + (\1_H * \1_W) > $8000)
+		; if inside VRAM space ($8000 - $A000), use SetVRAM to prevent
+		; access outside of vblank (else it will trash video display)
+			call	mem_SetVRAM
+		ELSE
+			call	mem_Set
+		ENDC
+	ENDC
+	; matrix-name, start addr of values, byte-count of values (end-start)
+	IF _NARG == 3	; if user passes start & end address of values to fill
+		; make pre-compile checks only
+		; if passed argument 3 isn't a dynamic value in a register pair
+		IF STRIN("afbcdehlAFBCDEHL", "\3") == 0
+		; verify matrix will be fully filled by supplied values
+		IF (\3) != (\1_H * \1_W)
+			PRINTT	"\nfilling matrix requires all values to be "
+			PRINTT	"specified. In this case, \1 is of size "
+			PRINTV	\3
+			PRINTT	" but you only provided values up to size "
+			PRINTV	\1_H * \1_W
+			FAIL	"\n"
+		ENDC
+		ENDC	; end pre-compile checks
+		load	de, \1, "matrix address is destination"
+		load	hl, \2, "start address of values is source"
+		load	bc, \3, "number of bytes to fill (end - start)"
+		IF (\1 < $A000) && (\1 + (\1_H * \1_W) > $8000)
+		; if inside VRAM space ($8000 - $A000), use SetVRAM to prevent
+		; access outside of vblank (else it will trash video display)
+			call	mem_CopyVRAM
+		ELSE
+			call	mem_Copy
+		ENDC
 	ENDC
 	ENDM
 
+; declare (set aside) variables in ram for iteration of a matrix.
+; variables are set up one-after another in ram so that they can be read
+; from a pointer, increment the pointer, then read next variable
 mat_IterDeclare: MACRO
-	var_LowRamByte	\1_iter_ptr	; pointer to (sub)matrix index
-	var_LowRamByte	\1_iter_ptr2	; msb of matrix pointer
+	var_LowRamByte	\1_iter_ptrL	; pointer to (sub)matrix index
+	var_LowRamByte	\1_iter_ptrH	; msb of matrix pointer
 	var_LowRamByte	\1_iter_steps_remain	; steps remaining in column
 	var_LowRamByte	\1_iter_step		; iterator step in x
 	var_LowRamByte	\1_iter_rowsteps_remain	; row steps remaining
@@ -68,18 +112,17 @@ mat_IterDeclare: MACRO
 
 
 ; initialize iterator. Save variables to ram
-; if you pass in no arguments (aside from matrix name) the iterator will be
-; set up to iterate the entire matrix
-; variables are set up one-after another in ram so that they can be read
-; from [de] pointer, increment de, then read next variable
+; if you pass in just the matrix name, the iterator will be set up to
+; iterate the entire matrix
+; TRASHES ALL REGISTERS
 mat_IterInit: MACRO
 	IF _NARG == 1
-	ld	bc, \1 - 1	; load address of matrix - 1
+	ld	hl, \1 - 1	; load address of matrix - 1
 				; (we increment before sampling)
-	lda	c	; load LSB of matrix address (-1)
-	ld	[\1_iter_ptr], a
-	lda	b	; load MSB of matrix address (-1)
-	ld	[\1_iter_ptr2], a
+	lda	l	; load LSB of matrix address (-1)
+	ld	[\1_iter_ptrL], a
+	lda	h	; load MSB of matrix address (-1)
+	ld	[\1_iter_ptrH], a
 	; store steps-remain as a way to count down until we reach end of row
 	; in this case, the width of the matrix -1
 	lda	\1_W - 1
@@ -98,11 +141,71 @@ mat_IterInit: MACRO
 	lda	\1_H - 1
 	ld	[\1_iter_rowsteps_remain], a
 	; rowstep is extra amount to increment to return to start of next row
-	; for a whole-matrix iteration, rowstep is 1, as that'll get us from
-	; the last element of row 1 to the first element of row2, for example.
+	; for a whole-matrix iteration, rowstep is 0, as iter_step will get us
+	; from the last element of row 1 to the first element of row2,
+	; for example. When dealing with a submatrix, this will be > 0
 	lda	1
 	ld	[\1_iter_rowstep], a	; store rowstep of 1
 	ENDC
+	; 5 args: name, startY, endY, startX, endX  (assumes x,y steps are 1)
+	IF _NARG == 5
+		load	b, \2, "arg2 mat_IterInit: Start Y"
+		load	c, \3, "arg3 mat_IterInit: End Y"
+		load	d, \4, "arg4 mat_IterInit: Start X"
+		load	e, \5, "arg5 mat_IterInit: End X"
+		ld	a, 1
+		ld	[\1_iter_step], a
+		lda	e
+		sub	d	; calculate endX - startX
+		ld	[\1_iter_steps_remain], a	; aka # of cols
+		ld	[\1_iter_W], a		; submatrix width
+		cpl
+		inc	a	; get negative width (-width) of submatrix
+		add	\1_W	; matrix_width - submatrix_width = rowstep
+		ld	[\1_iter_rowstep], a
+		lda	c
+		sub	b	; calculate endY - startY
+		dec	a	; rowsteps = # of rows -1
+		ld	[\1_iter_rowsteps_remain], a
+		ld	e, d	; move Start X to e
+		ld	d, b	; move Start Y to d
+		mat_IndexYX	\1, d, e	; get start index in HL
+						; (which trashes all registers)
+		ldhard	de, \1 - 1	; we get matrix address - 1 because
+			; iterator steps 1 over before sampling byte
+		add	hl, de	; submatrix_start = index + matrix_address
+		ld	a, l
+		ld	[\1_iter_ptrL], a
+		ld	a, h
+		ld	[\1_iter_ptrH], a ; store starting address in ram
+	ENDC
+	ENDM
+
+; setup iterator to iterate in a 3x3 square centered around given coordinates
+; currenty doesn't account for edges
+mat_IterInitRelativeNeighbors: MACRO
+	load	d, \2
+	load	e, \3	; D,E should contain Y, X, respectively
+	mat_IterInit	\1	; lazy setup of all variables
+			; WARNING: overwrite A & HL
+	dec	d
+	dec	e	; get address @ (Y-1, X-1)
+	mat_IndexYX	\1, d, e	; calculate index @ (Y-1, X-1)
+	load	bc, \1	; base address of matrix
+	add	hl, bc	; full address of matrix@(Y-1,X-1)
+	lda	l
+	ld	[\1_iter_ptrL], a
+	lda	h
+	ld	[\1_iter_ptrH], a	; store pointer to start of submatrix
+	ld	b, 3	; size of submatrix (both width and height)
+	lda	b	; size of submatrix
+	ld	[\1_iter_W], a
+	ld	[\1_iter_steps_remain], a
+	dec	a	; rows to traverse are 1 less than height of submatrix
+	ld	[\1_iter_rowsteps_remain], a
+	lda	\1_W
+	sub	b	; rowstep = (matrix width - submatrix width)
+	ld	[\1_iter_rowstep], a
 	ENDM
 
 ; call this macro to get the next element in an initiated matrix iterator
@@ -114,7 +217,7 @@ mat_IterInit: MACRO
 ; StopIteration signal).
 mat_IterNext: MACRO
 	; load pointer to current index within matrix
-	ld	de, \1_iter_ptr	; de points to LSB of address ptr to submatrix
+	ld	de, \1_iter_ptrL	; de points to LSB of address ptr to submatrix
 	call	mat_IterNext_fxn
 	ENDM
 
@@ -173,8 +276,8 @@ mat_IterNext_fxn:
 	add	hl, bc	; step to next row's element in iterator
 	; now we need to reset column-steps-remaining
 	pop	de
-	push	de	; retrieve (and re-store) address of \1_iter_ptr
-	increment	de	; point to \1_iter_ptr2
+	push	de	; retrieve (and re-store) address of \1_iter_ptrL
+	increment	de	; point to \1_iter_ptrH
 	increment	de	; point to \1_iter_steps_remain
 	ld	[de], a		; reset \1_iter_steps_remain in ram
 	jr	.iter_get_element
