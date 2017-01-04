3d2b608adbb2f1b2be797029c93b062bdf7250bf
Lance Kindle <lance.kindle@gmail.com>
2017-01-03 20:32:40 -0800

    display # of surrounding tiles when S/B pressed
    mines and matrix created and prefilled with a specific field
    pressing A will display a flag on the field.

--- minesweepGB.asm
+++ minesweepGB.asm
@@ -68,13 +68,57 @@ include "matrix.asm"
 
 
 	mat_Declare	_SCRN0, SCRN_VY_B, SCRN_VX_B	; setup screen matrix
+	mat_Declare	mines, SCRN_VY_B, SCRN_VX_B	; setup mines matrix
+	mat_Declare	flags, SCRN_VY_B, SCRN_VX_B	; setup flags matrix
 	; we'll now be able to use mat_GetYX and mat_SetYX on _SCRN0
 	; meaning we can address the background tiles like a 32x32 matrix
 	; SCRN_VY_B == 32 == SCRN_VX_B
+	mat_IterDeclare	mines	; declar ram variables for mines iterator
 
 Blank	SET	" "
 Mine	SET	"*"
 Flag	SET	"/"	; Blank, Mine, Flag are now variable constants
+
+
+
+populate_minefield: MACRO
+ DB   1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+ DB   0,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0
+ DB   0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0
+ DB   0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1
+ DB   0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0
+ DB   0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0
+ DB   0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0
+ DB   0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0
+ DB   0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0
+ DB   0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0
+ DB   0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+ DB   1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0
+ DB   0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+ DB   0,0,1,0,0,0,0,0,1,0,1,0,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0
+ DB   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+ DB   0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0
+ DB   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+ DB   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+ DB   0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0
+ DB   0,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0
+ DB   0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,1,0,0,0,0,0,0
+ DB   0,0,0,0,0,1,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1
+ DB   0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0
+ DB   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+ DB   1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,0,1,0,0,0,0,1
+ DB   0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0
+ DB   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0
+ DB   0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0
+ DB   0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0
+ DB   0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+ DB   0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0
+ DB   0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0
+	ENDM
+
+mines_prefill:
+	populate_minefield	; call macro paste minefield
+mines_prefill_end:
 
 
 ClearSpriteTable:
@@ -82,13 +126,6 @@ ClearSpriteTable:
 	ld	hl, OAMDATALOC
 	ld	bc, OAMDATALENGTH
 	call	mem_Set
-	ret
-
-LoadWords:
-	ld	hl, Title
-	ld	de, _SCRN0 + SCRN_VX_B * 5
-	ld	bc, TitleEnd - Title
-	call	mem_CopyVRAM
 	ret
 
 SpriteSetup:
@@ -111,20 +148,18 @@ begin:
 	call	LoadFont
 	call	ClearSpriteTable
 	call	lcd_On
-	call	LoadWords
 	call	SpriteSetup
 	call	lcd_ShowBackground
 	call	lcd_ShowSprites
 	call	lcd_EnableVBlankInterrupt
+	mat_Init	mines, mines_prefill, mines_prefill_end - mines_prefill
+	mat_Init	flags, Blank
 .mainloop:
 	call	lcd_Wait4VBlank
 	call	jpad_GetKeys  ; loads keys into register a, and jpad_rKeys
 	call	move_sprite_within_screen_bounds
-.past_operation:
-	lda	[jpad_rEdge]
-	and	PADF_B
-	if_flag	nz,	call toggle_flag
-
+	if_	jpad_EdgeB, call	toggle_flag
+	if_	jpad_EdgeA, call	probe_cell
 	jp	.mainloop; jr is Jump Relative (it's quicker than jp)
 
 
@@ -140,18 +175,67 @@ toggle_flag:
 	; so to translate from sprite coordinates to background coordinates,
 	; we divide by 8.
 	GetSpriteYAddr	Sprite0		; Y is loaded in a
-	shift_right	a
-	shift_right	a
-	shift_right	a	; A>>3 == A/8
+	math_Div	a, 8	; normally overwrites registers. But quick way
+				; doesn't
 	ld	d, a	; Y coordinate should be in D
 	GetSpriteXAddr	Sprite0		; X is loaded in a
-	shift_right	a
-	shift_right	a
-	shift_right	a	; A>>3 == A/8
+	math_Div	a, 8
 	ld	e, a	; X coordinate should be in E
 	mat_SetYX	_SCRN0, d, e, Flag
 	ret
 
+
+probe_cell:
+	; probe cell @ current location
+	GetSpriteYAddr	Sprite0		; Y is loaded in a
+	math_Div	a, 8
+	ld	d, a	; Y coordinate should be in D
+	GetSpriteXAddr	Sprite0		; X is loaded in a
+	math_Div	a, 8
+	ld	e, a	; X coordinate should be in E
+	push	de	; store Y,X for later use
+	mat_IndexYX	mines, d, e	; get address in HL
+	push	hl	; store current matrix index. We'll use it twice
+	mat_GetIndex	flags, hl	; result in a
+	pop	hl
+	pop	de
+	ifa	==, Flag, ret		; ignore probe if Y,X is flagged
+.continue_probe		; now we need to probe (y-1, x-1):(y+1,x+1)
+	push	hl	; store Y,X index for writing # to screen later
+	lda	-1
+	add	d	; get Y - 1
+	ifa	==, $FF, inc	a	; correct for underflow
+					; keeps y >= 0
+	ld	b, a	; store start Y
+	lda	2
+	add	d	; get Y + 2
+	ifa	>, SCRN_Y_B, dec	a	; keeps y <= screen height
+	ld	c, a	; store End Y
+	; Y-1:Y+2 is now loaded in B:C, respectively
+	lda	-1
+	add	e	; get X - 1
+	ifa	==, $FF, inc	a	; correct for underflow
+					; keeps x >= 0
+	ld	d, a	; store start X
+	lda	2
+	add	e	; get X + 2
+	ifa	>, SCRN_X_B, dec	a	; keeps x <= screen width
+	ld	e, a	; store end X
+	; X-1:X+2 is now loaded in D:E, respectively
+	; setup iterator @mines, from (y-1, x-1) to (y+1, x+1)
+	; aka	[y-1:y+2,x-1:x+2)    <==  [inclusive start, exclusive end)
+	; where (y,x) is the coordinates of the player
+	mat_IterInit	mines, b, c, d, e
+	mat_IterExhaust mines
+	;mat_IterCount	mines, ==, 1; counts mines surrounding player
+	; so far it seems like iterCount iterates over a smaller matrix
+	; (iter must not iterate correctly)
+	add	"0"			; set A to string version of count
+	pop	hl	; retrieve Index @ Y, X
+	push	af
+	call	lcd_Wait4VBlank		; need to wait for VRAM to be available
+	pop	af
+	mat_SetIndex	_SCRN0, hl, a	; write count to screen background
 	ret
 
 
@@ -159,19 +243,15 @@ move_sprite_within_screen_bounds:
 	; MoveIf* are macros from sprite.inc
 	; Only move if NOT on borders
 	GetSpriteXAddr	Sprite0
-	push af
+	push	af	; store X value for later
 	ifa	<, SCRN_X - 8,  MoveOnceIfRight Sprite0, 8
-	pop af
-	push af
+	pop	af
 	ifa	>, 0,		MoveOnceIfLeft	Sprite0, 8
-	pop af
 	GetSpriteYAddr	Sprite0
-	push af
+	push	af
 	ifa	<, SCRN_Y - 8,  MoveOnceIfDown	Sprite0, 8
-	pop af
-	push af
+	pop	af
 	ifa	>, 0,		MoveOnceIfUp	Sprite0, 8
-	pop af
 	ret
 
 
@@ -192,6 +272,8 @@ Title:  ; using (:) will save ROM address so that you can reference it in code
 	DB	"abcdefghijklmnopqrstuvwxyz"
 TitleEnd:
 
+; copies the dmacode to HIRAM. dmacode will get run each Vblank,
+; and it is resposible for copying sprite data from ram to vram.
 initdma:
 	ld	de, DMACODELOC
 	ld	hl, dmacode
