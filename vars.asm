3b6bfb2083f9562efa2dad8845cbc1e2ef32a72c
Lance Kindle <lance.kindle@gmail.com>
2017-01-10 03:40:43 -0800

    add LowRamWord functionality
    LowRamWord defines a start address of 2 bytes. A function is also
    provided to safely (without trashing other registers) load two
    registers at once from a LowRamWord.
    (also replaced some groups of spaces with tabs)

--- vars.asm
+++ vars.asm
@@ -2,8 +2,8 @@
 ; definitions and macros useful for creating variables pre-compiled,
 ; or variables residing in the gameboy ram
 
-        IF !DEF(VARS_ASM)
-VARS_ASM        SET     1
+	IF !DEF(VARS_ASM)
+VARS_ASM	SET     1
 
 include "gbhw.inc"
 
@@ -16,7 +16,7 @@ MidRamLimit     SET     MidRamBase + $0FFF ; 4096 bytes (var+space) available
 ; will then get copied to OAM location in Video-Ram [$FE00 - $FEA0)
 
 ; LowRamByte is a macro you can call to assign a variable a byte-address in ram
-; there's a LOT of available bytes: $C0A0 - $DFFF
+; there's a LOT of available bytes: $C0A0 - $C29F (512)
 ; each time you call LowRamByte, it assigns your variable an address, then
 ; increments. You should have no fear of running out of space
 ; I've set an arbitrary limit so that I can define other sections of memory
@@ -24,10 +24,20 @@ MidRamLimit     SET     MidRamBase + $0FFF ; 4096 bytes (var+space) available
 var_LowRamByte:     MACRO
 \1      EQU     LowRamBase      ; NOTICE I can't put a space before \1
 LowRamBase       Set     LowRamBase + 1
-        IF (LowRamBase >= LowRamLimit)
-                FAIL    "too many variables declared"
-        ENDC
-        ENDM
+	IF (LowRamBase >= LowRamLimit)
+		PRINTT "\n\1 was last LowRamByte declared. "
+		FAIL    "too many variables declared"
+	ENDC
+	ENDM
+
+var_LowRamWord: MACRO
+\1	EQU	LowRamBase
+LowRamBase	Set	LowRamBase + 2	; \1 points to two bytes
+	IF (LowRamBase >= LowRamLimit)
+		PRINTT	"\n\1 was last LowRamWord declared. "
+		FAIL    "too many variables declared"
+	ENDC
+	ENDM
 
 
 ; call var_MidRamBytes to allocate memory space for a variable.
@@ -35,15 +45,64 @@ LowRamBase       Set     LowRamBase + 1
 ; argument 2 is the size of the variable
 var_MidRamBytes: MACRO
 \1      EQU     MidRamBase
-        IF _NARG == 1
-                FAIL    "var_MidRamBytes requires a size argument"
-        ENDC
+	IF _NARG == 1
+		FAIL    "var_MidRamBytes requires a size argument"
+	ENDC
 MidRamBase      Set     MidRamBase + \2
-        IF (MidRamBase >= MidRamLimit)
-                PRINTT  "\n$"
-                PRINTV  MidRamBase
-                FAIL    "\nMidRam space depleted"
-        ENDC
-        ENDM
+	IF (MidRamBase >= MidRamLimit)
+		PRINTT  "\n$"
+		PRINTV  MidRamBase
+		FAIL    "\nMidRam space depleted"
+	ENDC
+	ENDM
 
-        ENDC    ; end vars.asm definitions
+
+; use this macro to load a word from ram. It loads the word into the specified
+; register without trashing any other registers (it will push / pop to do so)
+; var_GetLowRamWord	register_pair, variable_name
+; e.g. var_GetLowRamWord	b,c, player_coordinates
+; following Z80 stack-pointer convention, the LSB is stored @address,
+; and the MSB is stored @address + 1.
+; this means that the example above is psuedo-code for
+; ld	c, [player_coordinates]
+; ld	b, [player_coordinates + 1]
+; register A or F is not allowed because we make use of A
+; FAST:	17/14	17 cycles, 14 bytes
+var_GetLowRamWord: MACRO
+	IF STRIN("bcdehlBCDEHL", "\1") == 0
+		FAIL	"require a register (but not A). Got \1"
+	ENDC
+	IF STRIN("bcdehlBCDEHL", "\2") == 0
+		FAIL	"require a register (but not A). Got \2"
+	ENDC
+	push	af	; save AF (we have to use A)
+	lda	[\3]
+	ld	\2, a		; load LSB from address
+	lda	[\3 + 1]
+	ld	\1, a		; load MSB from address+1
+	pop	af
+	ENDM
+
+; same deal as var_GetLowRamWord. Stores register pair in memory
+; at specified address in ram. Sets LSB @address, MSB @address+1
+; doesn't trash registers
+; register A or F is not allowed because we make use of A
+; FAST:	17/14	17 cycles, 14 bytes
+var_SetLowRamWord: MACRO
+	IF STRIN("bcdehlBCDEHL", "\1") == 0
+		FAIL	"require a register (but not A). Got \1"
+	ENDC
+	IF STRIN("bcdehlBCDEHL", "\2") == 0
+		FAIL	"require a register (but not A). Got \2"
+	ENDC
+	push	af	; save AF (we have to use A)
+	lda	\2
+	ld	[\3], a		; store LSB at address
+	lda	\1
+	ld	[\3 + 1], a	; store MSB at address+1
+	pop	af
+	ENDM
+
+
+
+	ENDC    ; end vars.asm definitions
