39e5ea7ce1fa6aadb4e822e773bbbf23483cb99b
Lance Kindle <lance.kindle@gmail.com>
2017-02-02 09:39:54 -0800

    Initial debug module commit
    the debug module is to be used while runnning a game. Simply
    `call bug_snapshot` to write all registers AF, BC, DE, HL, SP, & PC
    to the address $FF04 (rDIV). Register pairs are written MSB first
    (so A before F, B before C, etc.). The routine bug_snapshot is written
    such that it will report SP and PC as they were BEFORE bug_snapshot
    was called. Additionally, it attempts to preserve and restore the
    interrupt and timer state (as best as possible) so that any-timer
    based interrupts / events will be minimally impacted.
    You'll need to use custom hardware or modify an emulator to preserve
    writes to $FF04 in a list in order to see the values.

---
+++ debug.asm
@@ -0,0 +1,76 @@
+; debug file is meant as a way to debug (LIVE!) running programs
+; pass bug_ macros the text you wish to display, and it'll be written
+; (in serial fashion) to rDIV, aka the address $FF04. Since most people don't
+; ever write to this register, I figure it's safe to use for debugging purposes
+
+; take a snapshot of the system registers
+; in order: af bc de hl  (yes, we also write the flags reg.)
+; finally, SP and PC
+; all of these register are written (one at a time, MSB first) to rDIV.
+; if you use hardware or software to monitor writes to this address, you
+; can capture the debug information.
+; This routine preserves all registers, AND preserves (as best as possible)
+; current interrupt and timer state.
+; This writes heavily to rDIV, which has the side effect of incrementing other
+; timers extremely quickly. To counteract this, I preserve interrupt flags
+; and the timer and restore them at the end.
+bug_snapshot:
+	di	; disable interrupts since we'll be doing some SP manipulation
+	push	af
+	ld	a, [rTIMA]	; preserve timer counter. We'll restore
+	push	af		; at end of snapshot
+	ld	a, [rIF]	; preserve interrupt flags. We'll restore
+	push	af		; at end of snapshot
+	push	hl
+	add	sp, 6		; backtrack past pushed HL & rIF & rTIMA values
+	pop	af		; get original AF
+	ld	[rDIV], a	; write A
+	add	sp, -1		; go forward 1 byte. To align SP with F (flags)
+				; (remember SP starts at top of memory)
+	pop	af		; now we pop off F? into AF
+	ld	[rDIV], a	; write F
+	; at this point, SP is +1 (1 byte before our pushed values began).
+	; it also is 1 byte into the return address (pushed when calling
+	; bug_snapshot)
+	; we've pushed a total of 8 bytes. So SP is 9 bytes away from top
+	; of where we want to be
+	add	SP, -1	; align SP to where it was before we pushed 8 bytes
+			; (so SP is 8 bytes from top). From here we can Pop the
+			; return address -- aka PC before snapshot was called
+	ld	a, b
+	ld	[rDIV], a	; write b
+	ld	a, c
+	ld	[rDIV], a	; write c
+	ld	a, d
+	ld	[rDIV], a	; write d
+	ld	a, e
+	ld	[rDIV], a	; write e
+	ld	a, h
+	ld	[rDIV], a	; write h
+	ld	a, l
+	ld	[rDIV], a	; write l
+	ld	hl, sp+2	; get SP address (before fxn call)
+				; +2 basically offsets so that HL now holds
+				; where SP would have pointed right before this
+				; routine was called
+	ld	a, h
+	ld	[rDIV], a	; write S
+	ld	a, l
+	ld	[rDIV], a	; write P
+			; (remember, we left SP ready to pop off return addr)
+	pop	hl	; return address == PC before bug_snapshot was called
+			; SP is now 10 bytes away from top (PC,AF,AF,AF,HL)
+	add	sp, -10	; return SP to top of stack, ready to pop
+	ld	a, h
+	ld	[rDIV], a	; write P
+	ld	a, l
+	ld	[rDIV], a	; write C
+	; that's it! We've written to rDIV our entire register stack, in order.
+	; AF, BC, DE, HL, SP, PC
+	pop	hl
+	pop	af		; restore original interrupt flags (so that we
+	ld	[rIF], a	; interrupts to pre-snapshot state)
+	pop	af
+	ld	[rTIMA], a	; restore timer to pre-snapshot state
+	pop	af	;get original AF values
+	reti		; return and enable interrupts
